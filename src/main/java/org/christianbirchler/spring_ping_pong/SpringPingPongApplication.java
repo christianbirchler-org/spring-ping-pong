d66d9b7139f493c7b4e91882ed4476ac847c4404
Christian Birchler <birchler.chr@gmail.com>
2025-03-06 15:58:06 +0100

    initial commit

---
+++ src/main/java/org/christianbirchler/spring_ping_pong/SpringPingPongApplication.java
@@ -0,0 +1,13 @@
+package org.christianbirchler.spring_ping_pong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPingPongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPingPongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/christianbirchler/spring_ping_pong/SpringPingPongApplicationTests.java
@@ -0,0 +1,13 @@
+package org.christianbirchler.spring_ping_pong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPingPongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
